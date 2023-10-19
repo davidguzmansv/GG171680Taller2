dce85cd26b200877b8c45d47129c6beb0d1125bb
Jocelyn.gil <anasulma1230@gmail.com>
2023-10-18 18:30:44 -0600

    Ocultar

--- src/sv/edu/udb/form/Cajero.java
+++ src/sv/edu/udb/form/Cajero.java
@@ -13,10 +13,10 @@ public class Cajero extends JFrame {
     private JButton crearClienteButton;
     private JButton btningresaraCuentas;
     private JButton salirButton;
-    private JTextField textField1;
-    private JTextField textField2;
-    private JTextField textField3;
-    private JTextField textField4;
+    private JTextField txtId;
+    private JTextField txtNombre;
+    private JTextField txtDui;
+    private JTextField txtPin;
     private JButton crearCuentaButton;
     private JButton retirosButton;
     private JButton abonosButton;
@@ -24,19 +24,19 @@ public class Cajero extends JFrame {
     private JButton detalleDeCuentasButton;
     private JButton transaccionesXCuentaButton;
     private JButton salirButton1;
-    private JTextField textField5;
+    private JTextField txtCuenta1;
     private JTextField textField6;
     private JButton agregarButton;
-    private JTextField textField7;
+    private JTextField txtCuenta2;
     private JTextField textField8;
     private JButton retirarButton;
-    private JTextField textField9;
+    private JTextField txtCuenta3;
     private JTextField textField10;
     private JButton abonarButton;
     private JTable table1;
     private JTable table2;
     private JTable table3;
-    private JTextField textField11;
+    private JTextField txtCuenta4;
     private JTextField textField12;
     private JComboBox cmbMenu1;
     private JTextField txtUsuario;
@@ -44,6 +44,14 @@ public class Cajero extends JFrame {
     private JLabel lblUsuario;
     private JPasswordField txtClave;
     private JButton btnIngresar;
+    private JLabel lblId;
+    private JLabel lblNombre;
+    private JLabel lblDui;
+    private JLabel lblPin;
+    private JLabel lblCuenta1;
+    private JLabel lblCuenta2;
+    private JLabel lblCuenta3;
+    private JLabel lblCuenta4;
 
     public Cajero(String title) {
         super(title);
@@ -52,6 +60,30 @@ public class Cajero extends JFrame {
         this.setMinimumSize(new Dimension(600, 500));
         this.setLocationRelativeTo(getParent());
         this.setSize(1300,600);
+
+        lblId.setVisible(false);
+        lblNombre.setVisible(false);
+        lblDui.setVisible(false);
+        lblPin.setVisible(false);
+        lblCuenta1.setVisible(false);
+        lblCuenta2.setVisible(false);
+        lblCuenta3.setVisible(false);
+        lblCuenta4.setVisible(false);
+        txtId.setVisible(false);
+        txtDui.setVisible(false);
+        txtNombre.setVisible(false);
+        txtPin.setVisible(false);
+        txtCuenta1.setVisible(false);
+        txtCuenta2.setVisible(false);
+        txtCuenta3.setVisible(false);
+        txtCuenta4.setVisible(false);
+        crearCuentaButton.setVisible(false);
+        retirosButton.setVisible(false);
+        abonosButton.setVisible(false);
+        consultaXCuentaButton.setVisible(false);
+        detalleDeCuentasButton.setVisible(false);
+        transaccionesXCuentaButton.setVisible(false);
+        salirButton1.setVisible(false);
 
         // crear objetos
         btningresaraCuentas = new JButton("Ingresar a cuentas");
@@ -70,9 +102,31 @@ public class Cajero extends JFrame {
             }
         }));
 
+        crearClienteButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) { crearClienteButtonActionPerformed(e); }
+        });
     }
 
-    private void btningresaraCuentasActionPerformed(ActionEvent e) {
+    private void crearClienteButtonActionPerformed(java.awt.event.ActionEvent e){
+        lblId.setVisible(true);
+        lblDui.setVisible(true);
+        lblNombre.setVisible(true);
+        lblCuenta4.setVisible(true);
+        lblPin.setVisible(true);
+        lblCuenta3.setVisible(true);
+        lblCuenta2.setVisible(true);
+        lblCuenta1.setVisible(true);
+        txtId.setVisible(true);
+        txtNombre.setVisible(true);
+        txtDui.setVisible(true);
+        txtPin.setVisible(true);
+        txtCuenta4.setVisible(true);
+        txtCuenta3.setVisible(true);
+        txtCuenta2.setVisible(true);
+        txtCuenta1.setVisible(true);
+    }
+    private void btningresaraCuentasActionPerformed(java.awt.event.ActionEvent e) {
         Datos datosCuenta = new Datos();
         if(!datosCuenta.validarUsuario(txtUsuario.getText(), new String((txtClave.getPassword())))) {
             //jOptionPane.showMessageDialog(null, "Usuario o contraseña no validos");
